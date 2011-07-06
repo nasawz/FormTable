83ffd2b6dc1bd13fde414a8081c6962054cf5453
nasa <nasa127@gmail.com>
2011-07-06 15:45:16 +0800

    Initial Commit

---
+++ FormTable/FormTableAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  FormTableAppDelegate.h
+//  FormTable
+//
+//  Created by zhe wang on 11-7-6.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FormTableAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
